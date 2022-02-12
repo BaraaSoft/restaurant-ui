832416d4a1ec235c81950ce757fae633a884aaf5
baraaabuzeid <baraa@aleph-labs.com>
2022-02-12 23:57:37 +0200

    add model response object type

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface IRestaurantsModel{
+    id:number;
+    name:string;
+    schedules?:ISchedules[]
+}
+
+export type WeekDay = "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun";
+
+export interface ISchedules{
+    id:number;
+    openTime:string;
+    closeTime:string;
+    day:WeekDay;
+    restaurant:IRestaurantsModel
+}
+
+export interface IAction<T>{
+    payload:T
+    type:any
+}
