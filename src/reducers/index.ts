26ea3fdcef81e8b2c6030b99d86595cb864681be
baraaabuzeid <baraa@aleph-labs.com>
2022-02-12 23:57:06 +0200

    implements restaurant basic reducer

---
+++ src/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import {
+   restaurants
+} from './restaurants.reducer'
+
+
+export default combineReducers({
+    restaurants
+})
---
+++ src/reducers/restaurants.reducer.ts
@@ -0,0 +1,15 @@
+import {ActionType} from 'actions';
+import {IRestaurantsModel,IAction} from 'types';
+import _ from 'lodash';
+
+
+
+
+export const restaurants = (state = [], action:IAction<IRestaurantsModel[]>) => {
+    switch (action.type) {
+        case ActionType.AllRestaurants:
+            return _.uniqBy([...state, action.payload], "id")
+        default:
+            return state
+    }
+}
