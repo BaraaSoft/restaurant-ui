02655cd9f35746c9237f07df9ee12e47c5fd43f1
baraaabuzeid <baraa@aleph-labs.com>
2022-02-13 19:13:58 +0200

    adding search input and time filter component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,55 @@
 import React from 'react';
-import { PageHeader } from 'antd';
+import { PageHeader,Input,Select,TimePicker} from 'antd';
 
+const {Search} = Input;
+const { Option } = Select;
+
+
+const options = [
+    {value: 'Mon', label: 'Mon'},
+    {value: 'Tue', label: 'Tue'},
+    {value: 'Wed', label: 'Wed'},
+    {value: 'Thu', label: 'Thu'},
+    {value: 'Fri', label: 'Fri'},
+    {value: 'Sat', label: 'Sat'},
+    {value: 'Sun', label: 'Sun'},
+]
+
+const renderOptions = (options:any[])=>{
+    return options.map(opt=>{
+        return (<Option value={opt.value}>{opt.value}</Option>)
+    })
+}
 
 const Header = (props:any):JSX.Element=>{
+    const onSearch = (e:any)=>{
+
+    }
     return (
           <PageHeader
                 className="site-page-header"
-                onBack={() => null}
-                title="Title"
-                subTitle="This is a subtitle"/>
+                title="Restaurants"
+                subTitle="Check for the available restaurants at given time"
+                 extra={[
+                      <Input.Group compact>
+                          
+                        <Select defaultValue="Mon">
+                            {renderOptions(options)}
+                         </Select>
+                          <TimePicker.RangePicker
+                            placeholder={["from","to"]}
+                          />
+                          <Search
+                            style={{width:500}}
+                            size="middle"
+                            placeholder="search by restaurant name..."
+                            allowClear
+                            enterButton="Search"
+                            onSearch={onSearch}/>
+                           
+                      </Input.Group>
+    
+                 ]}/>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css'
+import 'antd/dist/antd.min.css'
 import './index.css'
 /** Redux Setup **/
 import { Provider } from 'react-redux';
