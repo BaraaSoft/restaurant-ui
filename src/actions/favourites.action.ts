501d7a804e19e67182f0dc919ed87ff08fdb26c7
baraaabuzeid <baraa@aleph-labs.com>
2022-02-20 00:26:14 +0200

    refactor fav actions

--- src/actions/favourites.action.ts
+++ src/actions/favourites.action.ts
@@ -8,45 +8,31 @@ import {getPageInfo}from 'utils';
 import  _ from 'lodash';
 
 
-
-
-
-
-const _fetchFavGroups = _.memoize(
-    async (userId:number,dispatch:ThunkDispatch<IFavouriteModel, void, Action>)=>{
-        const response:any = await axiosRequest().get(`/user/${userId}/favourites`);
-        await dispatch({payload:response.data,type:ActionType.AllFavouriteGroups})
-
-        /* to load first favourite group items */
-        const firstFavGrpItems = response.data[0] 
-        console.log({firstFavGrpItems})
-        dispatch(fetchFavGroupItems(firstFavGrpItems?.id))
-        
-    }
-)
-
-const _fetchFavGroupItems = _.memoize(
-    async (favGrpId:number,userId:number,dispatch:ThunkDispatch<IFavouriteItemModel, void, Action>)=>{
-        const response:any = await axiosRequest().get(`/user/${userId}/favourites/${favGrpId}/items`);
-        dispatch({payload:response.data.map((x:any)=>({...x,favGrpId:favGrpId})),type:ActionType.AllFavouriteGroupItems})
-    }
-)
-
-
 export const fetchFavGroups = (userId:number = 1) => async (dispatch:ThunkDispatch<IFavouriteModel, void, Action>,getState:any) => {
-    _fetchFavGroups(userId,dispatch)
-
+    const response:any = await axiosRequest().get(`/user/${userId}/favourites`);
+    await dispatch({payload:response.data,type:ActionType.AllFavouriteGroups})
+    /* to load first favourite group items */
+    const firstFavGrpItems = response.data[0] 
+    console.log({firstFavGrpItems})
+    dispatch(fetchFavGroupItems(firstFavGrpItems?.id))
 }
 
 
 export const addFavouriteItem = (restaurant:IRestaurantsModel,favGrpId:number,userId:number = 1) => async (dispatch:ThunkDispatch<IFavouriteItemModel, void, Action>,getState:any) => {
-  
-   delete restaurant.pageNum;
-   delete restaurant.schedules;
-   const response:any = await axiosRequest().post(`/user/${userId}/favourites/${favGrpId}/items`,{restaurant}).then((e)=>console.log(e))
-    .catch((err)=> console.log(">> Error",err))
+
+   const response:any = await axiosRequest().post(`/user/${userId}/favourites/${favGrpId}/items`,{restaurant});
+   
 }
 
 export const fetchFavGroupItems = (favGrpId:number,userId:number = 1) => async (dispatch:ThunkDispatch<IFavouriteItemModel, void, Action>,getState:any) => {
-    _fetchFavGroupItems(favGrpId,userId,dispatch)
+     const response:any = await axiosRequest().get(`/user/${userId}/favourites/${favGrpId}/items`);
+     dispatch({payload:response.data.map((x:any)=>({...x,favGrpId:favGrpId})),type:ActionType.AllFavouriteGroupItems})
+}
+
+export const addNewFavGroup = (groupName:string,userId:number = 1) => async (dispatch:ThunkDispatch<IFavouriteModel, void, Action>,getState:any) => {
+    const res:any = await axiosRequest()
+        .post(`/user/${userId}/favourites`,{name:groupName})
+        .catch((err)=> console.log(">> Error",err))
+    const response:any = await axiosRequest().get(`/user/${userId}/favourites`);
+    await dispatch({payload:response.data,type:ActionType.AllFavouriteGroups})
 }
--- src/components/Fav.tsx
+++ src/components/Fav.tsx
@@ -3,7 +3,7 @@ import { Layout, Breadcrumb,Tabs,List,Input,Button,Divider} from 'antd';
 import {Header,ListView} from 'components';
 import { IFavouriteItemModel, IFavouriteModel, IReducer } from 'types';
 import { connect } from 'react-redux';
-import {fetchFavGroups,fetchFavGroupItems} from 'actions';
+import {fetchFavGroups,fetchFavGroupItems,addNewFavGroup} from 'actions';
 import { PlusOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 
@@ -33,20 +33,22 @@ export interface FavViewProps{
     favouriteGroups?:IFavouriteModel[]
     favouriteItems?:IFavouriteItemModel[]
     fetchFavGroupItems?:(favGrpId:number,userId?:number)=>void
+    addNewFavGroup?:(name:string,userId?:number)=>void
 }
 
 const FavView = (props:FavViewProps):JSX.Element=>{
     const [tabPosition,setTabPosition] = useState<number>(1);
     const [favName,setFavName] = useState<string>();
-    const {fetchFavGroups,favouriteGroups,favouriteItems,fetchFavGroupItems} =  props;
+    const {fetchFavGroups,favouriteGroups,favouriteItems,fetchFavGroupItems,addNewFavGroup} =  props;
+    const [favGrpCount,setFavGrpCount] = useState<number>(favouriteGroups?.length || 0);  
 
     useEffect(()=>{
         fetchFavGroups?.call(null)
     },[])
 
      useEffect(()=>{
-       
-    },[])
+       setFavGrpCount(favouriteGroups?.length || 0)
+    },[favouriteGroups])
 
     const onTabChanged = (favGrpId:any)=>{
         fetchFavGroupItems?.call(null,favGrpId)
@@ -54,28 +56,29 @@ const FavView = (props:FavViewProps):JSX.Element=>{
 
     const onAddNewFavouriteGroup = ()=>{
         if(favName){
-            console.log(favName)
+            addNewFavGroup?.call(null,favName)
+            setFavGrpCount((count)=>count+1)
+            setFavName('');
         }
     }
 
     return(
         <div style={{height:540,overflowY:'scroll'}} >
             <InputGroup>
-                <FavInupt onChange={(e)=>setFavName(e.target.value)} 
+                <FavInupt value={favName} onChange={(e)=>setFavName(e.target.value)} 
                     bordered={false} size="large"
                     placeholder="add new favourite group"/>
-                <Button onClick={onAddNewFavouriteGroup} 
+                <Button onClick={onAddNewFavouriteGroup} loading={favouriteGroups?.length!=favGrpCount}
                     size="middle" type="primary" shape="circle" 
                     icon={<PlusOutlined />}/>
             </InputGroup>
             <Divider/>
-            <Tabs tabPosition="left" onChange={onTabChanged} >
+            <Tabs tabPosition="left" onChange={onTabChanged}>
                 {favouriteGroups?.map((favGrp,index)=>{
                     const filteredList = favouriteItems?.filter(item=>item.favGrpId==favGrp.id) 
                     return(
                         <TabPane tab={favGrp.name} key={favGrp.id}>
                              <List
-                                loading={filteredList?.length == 0}
                                 dataSource={filteredList}
                                 renderItem={item => (
                                     <List.Item key={item.id}>
@@ -97,4 +100,4 @@ const mapStateToProps = ({favouriteGroups,favouriteItems}:IReducer) => {
 }
 
 
-export default connect(mapStateToProps,{fetchFavGroups,fetchFavGroupItems})(FavView)
+export default connect(mapStateToProps,{fetchFavGroups,fetchFavGroupItems,addNewFavGroup})(FavView)
