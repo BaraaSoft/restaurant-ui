20061c49daa73f03a5156c40992aa3b691a14132
baraaabuzeid <baraa@aleph-labs.com>
2022-02-19 20:38:23 +0200

    implements showing favourite items list

--- src/components/Fav.tsx
+++ src/components/Fav.tsx
@@ -1,18 +1,65 @@
-import React,{useState} from 'react';
-import { Layout, Breadcrumb,Tabs} from 'antd';
+import React,{useState,useEffect} from 'react';
+import { Layout, Breadcrumb,Tabs,List,Input,Button,Divider} from 'antd';
 import {Header,ListView} from 'components';
+import { IFavouriteItemModel, IFavouriteModel, IReducer } from 'types';
+import { connect } from 'react-redux';
+import {fetchFavGroups,fetchFavGroupItems} from 'actions';
+import { PlusOutlined } from '@ant-design/icons';
+
 
 const { TabPane } = Tabs;
 
-const FavView = ():JSX.Element=>{
-    const [tabPosition,setTabPosition] = useState<number>(1)
+export interface FavViewProps{
+    fetchFavGroups?:(userId?:number)=>void
+    favouriteGroups?:IFavouriteModel[]
+    favouriteItems?:IFavouriteItemModel[]
+    fetchFavGroupItems?:(favGrpId:number,userId?:number)=>void
+}
+
+const FavView = (props:FavViewProps):JSX.Element=>{
+    const [tabPosition,setTabPosition] = useState<number>(1);
+    const [favName,setFavName] = useState<string>();
+    const {fetchFavGroups,favouriteGroups,favouriteItems,fetchFavGroupItems} =  props;
+
+    useEffect(()=>{
+        fetchFavGroups?.call(null)
+    },[])
+
+     useEffect(()=>{
+       
+    },[])
+
+    const onTabChanged = (favGrpId:any)=>{
+        fetchFavGroupItems?.call(null,favGrpId)
+    }
+
+    const onAddNewFavouriteGroup = ()=>{
+        if(favName){
+            console.log(favName)
+        }
+    }
+
     return(
         <div style={{height:540,overflowY:'scroll'}} >
-            <Tabs tabPosition="left">
-                {Array(50).fill(0).map((x,index)=>{
+            <Input.Group style={{display:'flex',justifyContent:'center',alignItems:'center',marginBottom:'42px'}} >
+                <Input onChange={(e)=>setFavName(e.target.value)} bordered={false} size="large" style={{ marginRight:'12px',paddingLeft:'28px',width: 'calc(100% - 200px)',borderRadius:'10px',backgroundColor:'#FAFAFA' }} 
+                    placeholder="add new favourite group"/>
+                <Button onClick={onAddNewFavouriteGroup} size="middle" type="primary" shape="circle"  icon={<PlusOutlined />}  />
+            </Input.Group>
+            {/* <Divider/> */}
+            <Tabs tabPosition="left" onChange={onTabChanged} >
+                {favouriteGroups?.map((favGrp,index)=>{
+                    const filteredList = favouriteItems?.filter(item=>item.favGrpId==favGrp.id) 
                     return(
-                        <TabPane tab={`Tab ${index}`} key={index}>
-                            {`Tab Number ${index}`}
+                        <TabPane tab={favGrp.name} key={favGrp.id}>
+                             <List
+                                loading={filteredList?.length == 0}
+                                dataSource={filteredList}
+                                renderItem={item => (
+                                    <List.Item key={item.id}>
+                                        <div>{item.restaurant.name}</div>
+                                    </List.Item>)}
+                                />
                         </TabPane>
                     )
                 })}
@@ -22,4 +69,10 @@ const FavView = ():JSX.Element=>{
 }
 
 
-export default FavView
+const mapStateToProps = ({favouriteGroups,favouriteItems}:IReducer) => {
+    
+    return { favouriteGroups,favouriteItems }
+}
+
+
+export default connect(mapStateToProps,{fetchFavGroups,fetchFavGroupItems})(FavView)
